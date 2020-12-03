6bb8e5cd978d737c343014ec8c56d4691475f87b
[Aaron Lo] <[loaaron1337@gmail.com]>
2020-12-03 10:54:07 -0800

    Git project practice

---
+++ Git project test 2.py
@@ -0,0 +1,14 @@
+#Gitprojectpractice
+
+#Test to demonstrate git workflow
+
+def main():
+    pass
+#Find the fibonacci number
+n = int(input("What fibonacci number do u want?"))
+if n in [1, 2]:
+    print("The {n}stand fib number is 1")
+
+
+if __name__ ="__main__":
+    main()
